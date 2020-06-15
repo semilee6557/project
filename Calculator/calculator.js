67a7d00dfce12f5541e16fd38307bdcb26d40171
semi lee <semilee6557@gmail.com>
2020-06-14 22:01:54 -0700

    Complete simple website

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -10,18 +10,32 @@ app.get("/", function(req, res){
   res.sendFile(__dirname+"/index.html")
 });
 
+
 app.post("/", function(req, res){
 
-var num1 = Number(req.body.num1);
-var num2 = Number(req.body.num2);
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
 
-var result = num1 + num2;
+  var result = num1 + num2;
 
 
-res.send("Total is "+ result);
+  res.send("Total is "+ result);
 })
 
+app.get("/bmicalculator", function(req, res){
+  res.sendFile(__dirname+"/bmiCalculator.html")
+});
 
+app.post("/bmicalculator", function (req, res) {
+
+  var height = Number(req.body.height);
+  var weight = Number(req.body.weight);
+
+  var result = weight/(height*height);
+
+  res.send("Your BMI is " + result);
+
+});
 
 app.listen(port, function(){
   console.log("Calculator homeage!")
